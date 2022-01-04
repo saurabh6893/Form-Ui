ced9fa0060a4f9fa08e6fb1b02680a4b3eb9ed69
saurabh <saurabhkedar@gmail.com>
2022-01-04 18:23:56 +0530

    completed project

--- core.js
+++ core.js
@@ -5,7 +5,6 @@ const passone = document.getElementById('password')
 const passtwo = document.getElementById('password2')
 
 // for error messages
-
 function theError(input, message) {
   const formB = input.parentElement
   formB.className = 'box error'
@@ -14,17 +13,19 @@ function theError(input, message) {
 }
 
 //for success messages
-
 function theSuccess(input) {
   const formB = input.parentElement
   formB.className = 'box success'
 }
+
 // for email validation
-function emailValidation(eMail) {
+function emailValidation(input) {
   const td =
     /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()\.,;\s@\"]+\.{0,1})+[^<>()\.,;:\s@\"]{2,})$/
 
-  return td.test(String(eMail).toLocaleLowerCase())
+  if (td.test(input.value.trim())) {
+    theSuccess(input)
+  } else theError(input, 'The email is not valid')
 }
 
 //case fix
@@ -43,8 +44,28 @@ function basicChecks(inputArr) {
   })
 }
 
+//password verifier
+function passwordVerifier(a, b) {
+  if (a.value !== b.value) {
+    theError(b, 'the Passords donot match')
+  }
+}
+
+//length validation
+function lengthCheck(input, min, max) {
+  if (input.value.length < min) {
+    theError(input, `${caseFix(input)} must be more than ${min} characters`)
+  } else if (input.value.length > max) {
+    theError(input, `${caseFix(input)} must be less than ${max} characters`)
+  } else theSuccess(input)
+}
+
 //event Listening function
 form.addEventListener('submit', function (e) {
   e.preventDefault()
   basicChecks([uName, eMail, passone, passtwo])
+  lengthCheck(uName, 8, 15)
+  lengthCheck(passone, 8, 15)
+  emailValidation(eMail)
+  passwordVerifier(passone, passtwo)
 })
